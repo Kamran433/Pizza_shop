9072775a0f1bb93ba5e97f0db6b463294e17d5f4
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:28:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S, M, or L ")
+# add_pepperoni = input("Do you want pepperoni? Y or N ")
+# extra_cheese = input("Do you want extra cheese? Y or N ")
+# if size=="S":
+#     if add_pepperoni=="Y" and extra_cheese=="Y":
+#         print("Your final bill is: $18.")
+#     elif add_pepperoni=="Y" and extra_cheese=="N":
+#             print("Your final bill is: $17.")
+#     elif add_pepperoni=="N" and extra_cheese=="Y":
+#                 print("Your final bill is: $16.")
+#     elif add_pepperoni=="N" and extra_cheese=="N":
+#                     print("Your final bill is: $15.")
+
+# if size=="M":
+#     if add_pepperoni=="Y" and extra_cheese=="Y":
+#         print("Your final bill is: $24.")
+#     elif add_pepperoni=="Y" and extra_cheese=="N":
+#             print("Your final bill is: $23.")
+#     elif add_pepperoni=="N" and extra_cheese=="Y":
+#                 print("Your final bill is: $21.")
+#     elif add_pepperoni=="N" and extra_cheese=="N":
+#                     print("Your final bill is: $20.")
+                                
+# if size=="L":
+#     if add_pepperoni=="Y" and extra_cheese=="Y":
+#         print("Your final bill is: $29.")
+#     elif add_pepperoni=="Y" and extra_cheese=="N":
+#             print("Your final bill is: $28.")
+#     elif add_pepperoni=="N" and extra_cheese=="Y":
+#                 print("Your final bill is: $26.")
+#     elif add_pepperoni=="N" and extra_cheese=="N":
+#                     print("Your final bill is: $25.") 
